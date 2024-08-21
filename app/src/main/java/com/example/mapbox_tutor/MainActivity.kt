a971cf62a4c1abb8cd4492b61b08ff6c4b3fedfa
muh farid wajdi a <muhaidil754@gmail.com>
2024-08-21 18:49:20 +0800

    Map Marker

--- app/src/main/java/com/example/mapbox_tutor/MainActivity.kt
+++ app/src/main/java/com/example/mapbox_tutor/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mapbox_tutor
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -14,20 +15,46 @@ import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
+import com.mapbox.maps.plugin.gestures.addOnMapClickListener
 
-var mapView: MapView? = null
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mapView: MapView
+    private lateinit var pointAnnotationManager: PointAnnotationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
 
         mapView = findViewById(R.id.mapView)
 
-        mapView?.getMapboxMap()?.loadStyleUri(Style.STANDARD)
-        setContentView(R.layout.activity_main)
+        mapView.mapboxMap.loadStyleUri(Style.STANDARD){ style ->
+            style.addImage(
+                "default_marker",
+                BitmapFactory.decodeResource(resources, R.drawable.red_marker)
+            )
+            pointAnnotationManager = mapView.annotations.createPointAnnotationManager()
+
+        }
+        mapView.mapboxMap.addOnMapClickListener { point ->
+            addMarker(point.latitude(), point.longitude())
+            true
+        }
+
+        super.onCreate(savedInstanceState)
 
 
+    }
+    private fun addMarker(latitude: Double, longitude: Double) {
+        val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
+            .withPoint(com.mapbox.geojson.Point.fromLngLat(longitude, latitude))
+            .withIconImage("default_marker") // Ikon default, bisa diubah sesuai kebutuhan
+
+        pointAnnotationManager.create(pointAnnotationOptions)
     }
 
 }
